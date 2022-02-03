b3dad274016b59874900ca9da251c0a2c94b8d38
simolex <simolex@yandex.ru>
2022-02-03 17:03:33 +0300

    Простой вариант игры готов Необходимы правки: 1) Заменить цикл валидации на рекурсию 2) Вывести предупреждение если введено не число

--- script.js
+++ script.js
@@ -1,8 +1,51 @@
 "use strict";
 
 const game = function () {
+  const startRange = 1;
+  const endRange = 100;
+
+  const isNumber = function (num) {
+    num += "";
+    return !isNaN(parseFloat(num)) && isFinite(num);
+  };
+  const getNumber = function (num) {
+    return parseFloat(num);
+  };
+
   const startGame = function () {
-    console.log("Игра началась!");
+    const secretNumber = startRange + Math.floor(Math.random() * endRange);
+    console.log(secretNumber);
+    const gameRound = function () {
+      let requestNumber;
+
+      do {
+        requestNumber = prompt(`Угадай число от ${startRange} до ${endRange}`);
+      } while (requestNumber != null && !isNumber(requestNumber));
+
+      if (requestNumber == null) {
+        return null;
+      }
+      requestNumber = getNumber(requestNumber);
+      switch (true) {
+        case requestNumber == secretNumber:
+          console.log("Поздравляю, Вы угадали!!!");
+          break;
+        case requestNumber > secretNumber:
+          console.log("Загаданное число меньше");
+          gameRound();
+          break;
+        case requestNumber < secretNumber:
+          console.log("Загаданное число больше");
+          gameRound();
+          break;
+      }
+    };
+
+    const gameResult = gameRound();
+    if (gameResult == null) {
+      return;
+    }
+
     const repeatGame = confirm("Хотели бы сыграть еще?");
     if (repeatGame) {
       startGame();
